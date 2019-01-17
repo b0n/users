66b38ecdfb26b9bcb265fd1f382d061a845bda8a
Kousuke Taekuchi <kousuketakeuchi@gmail.com>
2019-01-18 08:53:29 +0900

    add: plugins funayaki/users

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,10 @@ use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
 
+/**
+ * http://example.com/admin/users/users
+ * http://example.com/admin/users/roles
+ */
 Router::plugin('Users', ['path' => '/'], function (RouteBuilder $route) {
     $route->prefix('admin', function (RouteBuilder $route) {
         $route->setExtensions(['json']);
